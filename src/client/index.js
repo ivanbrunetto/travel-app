e3c41e71c5d893697b8113deadb4aefac6b00436
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-08-17 16:48:54 -0300

    refactor css and html

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,9 @@ import { app } from './js/App';
 // css files
 import './styles/resets.scss';
 import './styles/core.scss';
+import './styles/index.scss';
+import './styles/add-trip-form.scss';
+import './styles/trip-card.scss';
 
 
 export {
