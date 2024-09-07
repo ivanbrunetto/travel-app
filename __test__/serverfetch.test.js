aafd477df44a0847407b81b5afb79f9e7126f74e
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-09-07 06:40:34 -0300

    improve server side tests

--- __test__/serverfetch.test.js
+++ __test__/serverfetch.test.js
@@ -6,52 +6,78 @@ import { expect, it, jest } from '@jest/globals';
 import { fetchData } from '../src/client/js/serverfetch';
 
 it('tests fetchData with network error', async () => {
-    global.fetch = jest.fn(() => Promise.reject({ message: 'error test' }));
+    const testEndpoint = '/test';
+    const expFetchUrl = `http://localhost:8000/api${testEndpoint}`;
 
+    const mockFetch = jest.fn(() => Promise.reject({ message: 'test-error-message' }));
+    global.fetch = mockFetch;
 
     const mockAlert = jest.fn();
     window.alert = mockAlert;
 
-    const res = await fetchData('');
+    const res = await fetchData(testEndpoint);
     expect(mockAlert.mock.calls[0][0]).toBe('Network error');
     expect(mockAlert.mock.calls).toHaveLength(1);
+    expect(mockFetch.mock.calls[0][0]).toBe(expFetchUrl);
+    expect(mockFetch.mock.calls).toHaveLength(1);
     expect(res).toBeUndefined();
 });
 
 it('tests fetchData with server error', async () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-        json: () => Promise.resolve({
-            error: 1,
-            message: 'test error message',
-            data: null
-        })
-    }));
+    const expServerResponse = {
+        error: 1,
+        message: 'test-message',
+        data: null
+    }
+
+    const expFetchResult = {
+        json: () => Promise.resolve(expServerResponse)
+    }
+
+    const testEndpoint = '/test';
+    const expFetchUrl = `http://localhost:8000/api${testEndpoint}`;
+
+    const mockFetch = jest.fn(() => Promise.resolve(expFetchResult));
+    global.fetch = mockFetch;
 
     const mockAlert = jest.fn();
     window.alert = mockAlert;
 
-    const res = await fetchData('');
+    const res = await fetchData(testEndpoint);
     expect(mockAlert.mock.calls[0][0]).toBe('There was a server processing error');
     expect(mockAlert.mock.calls).toHaveLength(1);
+    expect(mockFetch.mock.calls[0][0]).toBe(expFetchUrl);
+    expect(mockFetch.mock.calls).toHaveLength(1);
     expect(res).toBeUndefined();
 });
 
 
 it('tests fetchData with success case', async () => {
-    const expectedWeatherData = {
+    const expServerResponse = {
         error: 0,
         message: 'test message',
-        data: 'test data'
+        data: {
+            a: 'some test data',
+            b: 'etc..'
+        }
     }
 
-    global.fetch = jest.fn(() => Promise.resolve({
-        json: () => Promise.resolve(expectedWeatherData)
-    }));
+    const expFetchResult = {
+        json: () => Promise.resolve(expServerResponse)
+    }
+
+    const testEndpoint = '/test';
+    const expFetchUrl = `http://localhost:8000/api${testEndpoint}`;
+
+    const mockFetch = jest.fn(() => Promise.resolve(expFetchResult));
+    global.fetch = mockFetch;
 
     const mockAlert = jest.fn();
     window.alert = mockAlert;
 
-    const res = await fetchData('');
+    const res = await fetchData(testEndpoint);
     expect(mockAlert.mock.calls).toHaveLength(0);
-    expect(res).toBe(expectedWeatherData.data);
+    expect(mockFetch.mock.calls[0][0]).toBe(expFetchUrl);
+    expect(mockFetch.mock.calls).toHaveLength(1);
+    expect(res).toStrictEqual(expServerResponse.data);
 });
